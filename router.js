c5947b3c4b1faa536d47867adf561f8b26b8cf25
Ranjith Reynold <ranjithreynold75@gmail.com>
2018-02-22 16:20:31 +0530

    success

--- router.js
+++ router.js
@@ -5,6 +5,19 @@ var url="mongodb://zeus:alwaysforward1.@ds012058.mlab.com:12058/zeus"; //keep it
 var mc=m.MongoClient;
 
 
+
+var users={
+    user:{
+
+    },
+    status:{
+
+    }
+
+};
+
+
+
 var _db;
 
 
@@ -27,12 +40,36 @@ module.exports=function(app,io){
     io.on("connection",function(socket)
     {
         console.log("A user connected:" + socket.id);
+        socket.emit('message', {'id': socket.id});
 
         socket.join("zues_room");
 
-        socket.emit("notify",{'message':"Welcome to Golite"});
+        //socket.emit("notify",{'message':"Welcome to Zeus"});
+
+
+        socket.on('register', function (data) {
+            var d = JSON.parse(data);
+            console.log("registering user " + d.id);
+            users.user[d.no] = d.id;
+            var phoneno=d.no;
+
+
+
+            var db=_db.collection('zeus_users');
+            db.updateOne({_id:d.no},{$set:{status:"online"}});
+
+            console.log("user status:"+users);
+
+
+
+                // io.sockets.in("room-"+room).emit('notify',{'message':phoneno+" is online"});
+
+           console.log(users);
+
+        })
 
     });
+
 
     app.get("/",function(req,res){
         res.send("Welcome to zeus!!");
@@ -48,7 +85,7 @@ var data={
      };
         var h = _db.collection('zeus_users');
 
-        var cursor = h.find({_id: req.body.no});
+        var cursor = h.find({_id: req.body.id});
 
         cursor.count(function (err, c) {
             if (err)
@@ -109,5 +146,10 @@ var data={
 
 
     })
+
+app.post("/add_new_car",function(req,res){
+
+})
+
 
 }
