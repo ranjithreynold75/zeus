bca049d90dca71fe82a67e599dff74c28168562c
Ranjith Reynold <ranjithreynold75@gmail.com>
2018-02-22 16:40:31 +0530

    success

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ function notify(req,res,next)
 require('./router')(app,io);
 app.use(notify);
 app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({ extended: false }));
+//app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.urlencoded({limit: '50mb', extended: true}));
 
 var server=http.listen(process.env.PORT || 5000,function(){
     console.log("server running in port "+(process.env.PORT || 5000));
