55849c420f194a35e9665e167773ab33bd924cd2
Ranjith Reynold <ranjithreynold75@gmail.com>
2018-02-28 13:51:29 +0530

    new try

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(notify);
 app.use(bodyparser.json());
 //app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.urlencoded({limit: '50mb', extended: true}));
-require('./router')(app,io);
+require('./route')(app,io);
 
 var server=http.listen(process.env.PORT || 5000,function(){
     console.log("server running in port "+(process.env.PORT || 5000));
